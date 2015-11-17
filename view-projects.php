bd4e2a773cdcca9b872b0be639ae6aaddba1a3a9
gohai <gottfried.haider@gmail.com>
2015-11-17 12:12:41 +0100

    Add title

--- view-projects.php
+++ view-projects.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<title>The Sausage Machine</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/primer.css">
