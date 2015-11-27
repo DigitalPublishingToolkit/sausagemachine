37fefd6eafcdef7b0aad78a0b8431843c5b4f773
gohai <gottfried.haider@gmail.com>
2015-11-27 11:37:03 +0100

    Add reminder

--- makefile.inc.php
+++ makefile.inc.php
@@ -69,6 +69,7 @@ function make_get_targets($dir) {
 function make_run($dir, $target = 'all', &$out = '') {
 	$old_cwd = getcwd();
 	@chdir($dir);
+	// XXX: support more than one target
 	@exec('make ' . escapeshellarg($target) . ' 2>&1', $out, $ret_val);
 	@chdir($old_cwd);
 	$out = implode("\n", $out);
