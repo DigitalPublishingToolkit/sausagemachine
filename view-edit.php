10bfb336ce4c7e9f4c2dc4c7a6170fa6bde6f429
gohai <gottfried.haider@gmail.com>
2015-11-27 11:50:19 +0100

    Temporarily revert 28656c94f345f79deecb6940deaf8e0685a45b70

--- view-edit.php
+++ view-edit.php
@@ -47,12 +47,12 @@
 
 			var hash = window.location.hash.substring(1);
 			if (hash != sessionStorage.getItem('tmp_key')) {
-				if (hash.length) {
+				//if (hash.length) {
 					sessionStorage.clear();
 					sessionStorage.setItem('tmp_key', hash);
-				} else {
-					window.location.hash = '#' + sessionStorage.getItem('tmp_key');
-				}
+				//} else {
+				//	window.location.hash = '#' + sessionStorage.getItem('tmp_key');
+				//}
 			}
 
 			if (sessionStorage.markdown) {
